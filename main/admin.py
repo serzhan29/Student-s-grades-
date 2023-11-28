cde21f51f6ff2abc93a0eff106b7c894f239ec3f
serzhan29 <133089140+serzhan29@users.noreply.github.com>
2023-11-28 21:51:20 +0600

    Update admin.py
    new_admin

--- main/admin.py
+++ main/admin.py
@@ -11,7 +11,7 @@ admin.site.register(Student, StudentAdmin)
 
 
 class SRS_ONE(admin.ModelAdmin):
-    list_display = ("id", "student", "subject", "week1", "week2", "week3", "week4", "week5", "week6", "week7", "SRS_1", )
+    list_display = ("id", "student", "subject", "week1", "week2", "week3", "week4", "week5", "week6", "week7", "week71", )
     list_display_links = ("id", "student", "subject", )
     search_fields = ("id","student", "subject")
     #list_filter = ['student', 'subject']
@@ -20,7 +20,7 @@ admin.site.register(SRSone, SRS_ONE)
 
 
 class SRS_TYO(admin.ModelAdmin):
-    list_display = ("id", "student", "subject", "week8", "week9", "week10",  "week11",  "week12",  "week13",  "week14", "week15", "SRS_2", )
+    list_display = ("id", "student", "subject", "week8", "week9", "week10",  "week11",  "week12",  "week13",  "week14", "week15", "week16", )
     list_display_links = ("id", "student", "subject", )
     search_fields = ['id', 'student', 'subject']
     #list_filter = ['student', 'subject']
