046788cc07bbf46f5e2cf4e223db45260529da67
serzhan29 <133089140+serzhan29@users.noreply.github.com>
2023-11-28 21:50:30 +0600

    Update urls.py
    new_urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from .views import StudentListView, StudentDetailView, \
     MainListView, TeacherProfileView, StudentProfileView, \
     login_view, logout_view, TeacherGroupView, \
-    StudentGradesView, teacher_subject_grades, get_grade, save_grade
+    StudentGradesView, teacher_subject_grades, \
+    update_grade, calculate_and_save_result, calculate_and_save_result_srsone
 
 
 urlpatterns = [
@@ -14,8 +15,11 @@ urlpatterns = [
     path('<slug:slug>/', StudentDetailView.as_view(), name='table2'),
     path('grades/<slug:slug>/', StudentGradesView.as_view(), name='grades'),
     path('teacher/<str:teacher_url>/<str:group_link>/grades/', teacher_subject_grades, name='teacher_subject_grades'),
-    path('get_grade/', get_grade, name='get_grade'),
-    path('save_grade/', save_grade, name='save_grade'),
+    path('teacher/<str:teacher_url>/<str:group_link>/grades/update_grade/', update_grade, name='update_grade'),
+    path('teacher/<slug:teacher_url>/<slug:group_link>/grades/calculate/', calculate_and_save_result,
+         name='calculate_and_save_result'),
+    path('teacher/<slug:teacher_url>/<slug:group_link>/grades/calculate_srsone/', calculate_and_save_result_srsone,
+         name='calculate_and_save_result_srsone'),
     path('teacher_profile/<slug:slug>/', TeacherProfileView.as_view(), name='teacher_profile'),
     path('student_profile/<slug:slug>/', StudentProfileView.as_view(), name='student_profile'),
     path('__debug__/', include('debug_toolbar.urls')),
